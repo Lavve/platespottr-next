46549c523baa4140ee61b0365bdedcee0c275c9b
Magnus Claesson <magnus.claesson@gmail.com>
2025-07-26 14:59:50 +0200

    chore: refactor sync-manifest-version.js to use ES module imports

--- scripts/sync-manifest-version.js
+++ scripts/sync-manifest-version.js
@@ -1,7 +1,5 @@
-// biome-ignore lint/style/useNodejsImportProtocol: Node.js script for automation
-const fs = require('fs')
-// biome-ignore lint/style/useNodejsImportProtocol: Node.js script for automation
-const path = require('path')
+import fs from 'node:fs'
+import path from 'node:path'
 
 const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'))
 const manifestPath = path.resolve(__dirname, '../public/manifest.json')
