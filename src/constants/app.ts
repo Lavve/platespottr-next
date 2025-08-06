04432e184a9ce206beb7b102617d1d1d394d7794
Magnus Claesson <magnus.claesson@gmail.com>
2025-08-06 13:19:15 +0200

    fix: update VIBRATE_MAX_MULTIPLYER constant from 4 to 10 for improved vibration functionality

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -5,7 +5,7 @@ export const HOLD_DURATION = 1000
 export const VIBRATE_SUBTILE = 3
 export const VIBRATE_SUCCESS = 100
 export const VIBRATE_ALERT = [20, 100, 200]
-export const VIBRATE_MAX_MULTIPLYER = 4
+export const VIBRATE_MAX_MULTIPLYER = 10
 export const DISABLE_REFRESH_REQUESTS_TIME = 20
 
 // Local storage keys
